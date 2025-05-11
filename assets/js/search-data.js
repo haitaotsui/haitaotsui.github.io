9a19afbe0fa9987b5685506ea94d81cdb0d1b4d0
haitaotsui <wangdadadake@gmail.com>
2025-05-11 01:48:41 +0000

    Deploying to gh-pages from @ haitaotsui/haitaotsui.github.io@6f2b894551e963f0827a58756f1e3e6178022ad5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,14 +10,14 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "",
           section: "Navigation",
           handler: () => {
