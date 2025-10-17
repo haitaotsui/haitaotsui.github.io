4e8c364fa2ab0edead2c55a91731083e267fd390
haitaotsui <wangdadadake@gmail.com>
2025-10-17 17:06:33 +0000

    Deploying to gh-pages from @ haitaotsui/haitaotsui.github.io@ec03627210e748dffef8099037f293de0e771c42 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,8 +423,8 @@ ninja.data = [{
           description: "Credit card and debit card transaction services.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-qi-sun-amp-haitao-xu-39-s-research-lab",
-          title: 'Qi Sun &amp;amp; Haitao Xu&amp;#39;s Research Lab',
+            },},{id: "projects-algorithms-amp-applications-lab",
+          title: 'Algorithms &amp;amp; Applications Lab',
           description: "A portfolio-style hub for algorithms and applied AI research.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
